c5a0f6c600de5a5e5ff6b41154643c8c4741cd34
Marco Friso <marco.friso@gmail.com>
2024-10-17 14:22:59 +0200

    feat: add RadioButtons, reset button in Home

---
+++ app/components/RadioButtons.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+interface RadioButtonsProps {
+  areas: string[];
+  setSelectedArea: (area: string) => void;
+  selectedArea: string;
+}
+
+const RadioButtons: React.FC<RadioButtonsProps> = ({
+  areas,
+  selectedArea,
+  setSelectedArea,
+}) => {
+  const handleAreaChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedArea(event.target.value);
+  };
+
+  return (
+    <div className="flex gap-2 text-slate-800">
+      <p className="font-bold mr-2">Filter by area:</p>
+      {areas.map((area) => (
+        <label key={area}>
+          <input
+            type="radio"
+            name="area"
+            value={area}
+            checked={selectedArea === area}
+            onChange={handleAreaChange}
+            className="mr-1"
+          />
+          {area}
+        </label>
+      ))}
+    </div>
+  );
+};
+
+export default RadioButtons;
--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,38 @@
 import { useState } from "react";
 import SearchBar from "./components/SearchBar";
 import Tabs from "./components/Tabs";
+import RadioButtons from "./components/RadioButtons";
 
 const Home = () => {
   const [query, setQuery] = useState<string>("");
+  const [selectedArea, setSelectedArea] = useState<string>("World");
 
   const handleTabChange = (selectedTab: string) => {
     console.log("Selected tab:", selectedTab);
   };
 
+  const handleReset = () => {
+    setQuery("");
+    setSelectedArea("World");
+  };
+
   return (
     <div className="mx-auto max-w-[1000px]">
-      <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-8 sm:p-20">
-        <h1 className="text-5xl font-bold text-w">Library</h1>
+      <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-6 sm:p-20">
+        <h1 className="text-5xl font-black text-w">Library</h1>
         <p>Browse for assets needed to report and present analysis</p>
         <SearchBar query={query} setQuery={setQuery} />
+        <RadioButtons
+          areas={["World", "EMEA", "APAC", "LATAM", "NA"]}
+          setSelectedArea={setSelectedArea}
+          selectedArea={selectedArea}
+        />
+        <button
+          className="rounded-lg font-bold bg-slate-700 text-slate-200 px-4 py-1"
+          onClick={handleReset}
+        >
+          Reset
+        </button>
         <Tabs
           tabs={["Featured", "KPI", "Layouts", "Storyboards"]}
           onTabChange={handleTabChange}

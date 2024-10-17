92733b9ac9152173b88f42dfcfffc80d00b7f10f
Marco Friso <marco.friso@gmail.com>
2024-10-17 20:10:39 +0200

    feat(Home): add filter functionality

--- app/components/Featured.tsx
+++ app/components/Featured.tsx
@@ -3,10 +3,34 @@ import type { Asset } from "@/content/types";
 import Modal from "./Modal";
 import AssetComponent from "./Asset";
 
-const Featured = ({ assets }: { assets: Asset[] }) => {
-  const [selectedAsset, setSelectedAsset] = useState<Asset | null>(null);
+const Featured = ({
+  assets,
+  selectedAssets,
+  setSelectedAssets,
+}: {
+  assets: Asset[];
+  selectedAssets: Asset[];
+  setSelectedAssets: (assets: Asset[]) => void;
+}) => {
   const [clickedAsset, setClickedAsset] = useState<Asset | null>(null);
   const [isOpen, setIsOpen] = useState(false);
+
+  const handleDivClick = (asset: Asset) => {
+    setIsOpen(true);
+
+    const isSelected = selectedAssets.includes(asset);
+    if (isSelected) {
+      setSelectedAssets(selectedAssets.filter((a) => a.id !== asset.id));
+    } else {
+      setSelectedAssets([...selectedAssets, asset]);
+    }
+  };
+
+  const handleCheckboxClick = (event: React.MouseEvent, asset: Asset) => {
+    event.stopPropagation();
+
+    setClickedAsset(asset);
+  };
 
   return (
     <div className="w-full grid md:grid-cols-2 gap-4">
@@ -17,12 +41,20 @@ const Featured = ({ assets }: { assets: Asset[] }) => {
         <div
           key={asset.id}
           className="flex flex-col p-4 bg-white rounded-lg border text-slate-600 w-full cursor-pointer hover:shadow-lg"
-          onClick={() => {
-            setIsOpen(true);
-            setClickedAsset(asset);
-          }}
+          onClick={() => handleDivClick(asset)}
         >
-          <p className="text-md font-semibold">{asset.name}</p>
+          <div
+            className="flex gap-3 w-fit"
+            onClick={(event) => handleCheckboxClick(event, asset)}
+          >
+            <input type="checkbox" id={`asset ${asset.id}`} />
+            <label
+              htmlFor={`asset ${asset.id}`}
+              className="text-md font-semibold"
+            >
+              {asset.name}
+            </label>
+          </div>
           <p className="text-sm line-clamp-3 text-ellipsis">
             {asset.description}
           </p>
--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import RadioButtons from "./components/RadioButtons";
 import Tab from "./components/Tab";
 import { assets } from "@/content/constants";
 import Featured from "./components/Featured";
+import { Asset } from "@/content/types";
 
 const areas = ["World", "EMEA", "APAC", "LATAM", "NA"];
 const tabs = ["Featured", "KPI", "Layouts", "Storyboards"];
@@ -15,11 +16,24 @@ const Home = () => {
   const [query, setQuery] = useState<string>("");
   const [selectedArea, setSelectedArea] = useState<string>(areas[0]);
   const [activeTab, setActiveTab] = useState<string>(tabs[0]);
+  const [selectedAssets, setSelectedAssets] = useState<Asset[]>([]);
 
   const handleReset = () => {
     setQuery("");
+    setSelectedAssets([]);
     setSelectedArea("World");
   };
+
+  const filteredAssets = assets.filter((asset) => {
+    if (selectedAssets.length > 0) {
+      setSelectedAssets([]);
+    }
+
+    return (
+      asset.name.toLowerCase().includes(query.toLowerCase()) &&
+      (selectedArea === "World" || asset.areas.includes(selectedArea))
+    );
+  });
 
   return (
     <div className="mx-auto max-w-[1000px]">
@@ -44,7 +58,11 @@ const Home = () => {
           setActiveTab={setActiveTab}
         />
         <Tab active={activeTab === "Featured"}>
-          <Featured assets={assets} />
+          <Featured
+            assets={filteredAssets}
+            selectedAssets={selectedAssets}
+            setSelectedAssets={setSelectedAssets}
+          />
         </Tab>
         <Tab active={activeTab === "KPI"}>BB</Tab>
         <Tab active={activeTab === "Layouts"}>CC</Tab>
